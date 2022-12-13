0dfaa2206ac222979afb544064c1447f0365a898
Daf <dafne.azzolina@gmail.com>
2022-12-13 19:47:06 -0300

    Finish header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,50 @@
 import React from 'react'
 import { SocialIcon } from 'react-social-icons';
+import { motion } from "framer-motion"
 
 type Props = {}
 
 const Header = (props: Props) => {
     return (
-        <header className="flex flex-row flex-around">
-            <div className="flex flex-row items-center">
-                {/* Social media icons */}
+        <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+            <motion.div
+                initial={{
+                    x: -500,
+                    opacity: 0,
+                    scale: 0.5
+                }}
+                animate={{
+                    x: 0,
+                    opacity: 1,
+                    scale: 1
+                }}
+                transition={{
+                    duration: 1.5
+                }}
+                className="flex flex-row items-center">
                 <SocialIcon url="https://github.com/Daf98" fgColor="gray" bgColor="transparent" />
-                <SocialIcon url="https://github.com/Daf98" fgColor="gray" bgColor="transparent" />
-                <SocialIcon url="https://github.com/Daf98" fgColor="gray" bgColor="transparent" />
-                <SocialIcon url="https://github.com/Daf98" fgColor="gray" bgColor="transparent" />
-            </div>
-            <div>
-            <SocialIcon className="cursor-pointer" network="email" fgColor="gray" bgColor="transparent" />
-            <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Contact Me</p>
-            </div>
+                <SocialIcon url="https://linkedin.com/u/dafne-azzolina" fgColor="gray" bgColor="transparent" />
+                <SocialIcon url="https://twitter.com/dafne-azzolina" fgColor="gray" bgColor="transparent" />
+                <SocialIcon url="https://angel.co/u/dafne-azzolina" fgColor="gray" bgColor="transparent" />
+            </motion.div>
+            <motion.div
+                initial={{
+                    x: 500,
+                    opacity: 0,
+                    scale: 0.5
+                }}
+                animate={{
+                    x: 0,
+                    opacity: 1,
+                    scale: 1
+                }}
+                transition={{
+                    duration: 1.5
+                }}
+                className="flex flex-row items-center text-gray-400 cursor-pointer">
+                <SocialIcon className="cursor-pointer" network="email" fgColor="gray" bgColor="transparent" />
+                <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Contact Me</p>
+            </motion.div>
         </header>
     )
 }
