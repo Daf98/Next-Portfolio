468172c93bf8377170048fe9f72cc2812db7ec3c
Daf <dafne.azzolina@gmail.com>
2022-12-13 23:41:56 -0300

    Style skills section

--- components/About.tsx
+++ components/About.tsx
@@ -10,7 +10,7 @@ const About = (props: Props) => {
         whileInView={{opacity: 1}}
         transition={{duration: 1.5}}
         className="flex flex-col relative h-screen text-center md:flex-row md:text-left max-w-7xl px-10 justify-evenly mx-auto items-center">
-            <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">About</h3>
+            <h3 className="absolute top-24 uppercase tracking-[20px] text-[#5ceacd] text-2xl">About</h3>
             <motion.div
                 initial={{
                     x: -200,
@@ -29,7 +29,7 @@ const About = (props: Props) => {
                 className="space-y-10 px-0 md:px-10"
             >
                 <h4 className="text-4xl font-semibold">Hi! My name is Daf and I'm a software developer</h4>
-                <p className="text-base"
+                <p className="text-base text-justify"
                 >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </motion.div>
         </motion.div>
---
+++ components/Skills.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+import Skill from './Skill'
+
+type Props = {}
+
+const Skills = (props: Props) => {
+  return (
+    <motion.div
+    initial={{opacity: 0}}
+    whileInView={{opacity: 1}}
+    transition={{duration: 1.5}}
+    className="relative flex flex-col text-center md:text-left max-w-[2000px] xl:px-10 min-h-screen justify-center gap-[10px] mx-auto items-center">
+        <h3 className="absolute top-24 uppercase tracking-[20px] text-[#5ceacd] text-2xl">Skills</h3>
+        <h3 className="absolute top-36 uppercase tracking-[1px] text-gray-500 text-sm">Here's a list of technologies I've worked with</h3>
+        <div className="grid grid-cols-4 gap-5">
+            <Skill directionLeft={true} />
+            <Skill directionLeft={true} />
+            <Skill directionLeft={true} />
+            <Skill directionLeft={true} />
+            <Skill directionLeft={true} />
+            <Skill directionLeft={true} />
+            <Skill directionLeft={true} />
+            <Skill directionLeft={true} />
+            <Skill directionLeft={false} />
+            <Skill directionLeft={false} />
+            <Skill directionLeft={false} />
+            <Skill directionLeft={false} />
+        </div>
+
+    </motion.div>
+  )
+}
+
+export default Skills
