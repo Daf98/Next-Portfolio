5ab8d260f024df8cd085152e2a70caa8b9f465a3
Daf <dafne.azzolina@gmail.com>
2022-12-13 21:41:04 -0300

    Finish styling hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,24 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-[#0a192f] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>Daf's Portfolio</title>
         <link rel="icon" href="./images/favicon.ico" />
       </Head>
       <Header />
-      {/* Header */}
       {/* Hero */}
+      <section id="hero" className="snap-center">
+        <Hero />
+      </section>
       {/* About */}
       {/* Experiences */}
       {/* Skills */}
       {/* Projects */}
       {/* Contact Me */}
-  </div>
+    </div>
   )
 }
